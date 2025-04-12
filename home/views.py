afd8f78ba603eba06b5c19875676dd3c27d6c3b2
Elmira <elmira.nikrioodi96@gmail.com>
2025-04-12 14:50:14 +0330

    Add contact form and email reply feature

--- home/views.py
+++ home/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-from django.views import View
+from django.views import View 
+from django.views.generic.edit import CreateView 
+from .forms import ContactForm
+from django.contrib import messages
+from .models import Contact
+
 # Create your views here.
 
 class HomeView(View):
     template_name = 'home/index.html'
+    form_class = ContactForm  
 
     def get(self,requset):
-        return render(requset , self.template_name)
+        form = self.form_class()
+        return render(requset , self.template_name , {'form':form})
     
+class ContactView(CreateView):
+    model = Contact
+    form_class = ContactForm   
+    template_name = 'home/index.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        messages.success(self.request, "پیام شما با موفقیت ارسال شد 😊")
+        return super().form_valid(form)
+    
+
+
 
