50a28c9be4eca0842b28686b92319919b5d845f9
Elmira <elmira.nikrioodi96@gmail.com>
2025-04-12 22:04:45 +0330

    Completed

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .views import HomeView , ContactView
 app_name = 'home'
 
 urlpatterns = [
+    path('home/', HomeView.as_view() , name='home'),
     path('', HomeView.as_view() , name='home'),
     path('contact/', ContactView.as_view(), name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,16 +4,19 @@ from django.views.generic.edit import CreateView
 from .forms import ContactForm
 from django.contrib import messages
 from .models import Contact
+from blog.models import Post
 
 # Create your views here.
 
 class HomeView(View):
     template_name = 'home/index.html'
     form_class = ContactForm  
+    
 
     def get(self,requset):
+        post = Post.objects.filter(status = 'p')
         form = self.form_class()
-        return render(requset , self.template_name , {'form':form})
+        return render(requset , self.template_name , {'form':form , 'posts':post})
     
 class ContactView(CreateView):
     model = Contact
